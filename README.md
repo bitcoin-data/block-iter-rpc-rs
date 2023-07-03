b0fe731165eea734ac60596ee06abf1fdf4c0f57
0xb10c <b10c@b10c.me>
2023-07-03 19:40:37 +0200

    add: print txids for certain p2tr tx as example

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # block-iter-rpc-rs
 A quick and dirty block and transaction iterator (using Bitcoin Core RPCs) that can be used to collect data and stats. Written in Rust.
+
+The provided example script can be run with an RPC username and password as follows:
+
+```
+$ cargo run <username> <password>
+```
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
+use std::env;
+
 use bitcoincore_rpc::{Auth, Client, RpcApi};
-use bitcoin_pool_identification::PoolIdentification;
+use rawtx_rs::input::InputType;
+use rawtx_rs::output::OutputType;
+use rawtx_rs::tx::TxInfo;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 + 1 {
+        println!("Expected exactly two arguments: RPC username and RPC password");
+        return;
+    }
+    let rpc_user = &args[1];
+    let rpc_password = &args[2];
+
     let rpc = Client::new(
         "http://127.0.0.1:8332",
-        Auth::UserPass("user".to_string(), "pass".to_string()),
+        Auth::UserPass(rpc_user.to_string(), rpc_password.to_string()),
     )
     .unwrap();
 
-    let start_height = 700_000;
+    let start_height = 780_000;
 
     let end_height = rpc.get_block_count().unwrap();
 
     for height in start_height..end_height {
         let hash = rpc.get_block_hash(height).unwrap();
         let block = rpc.get_block(&hash).unwrap();
-        let pool = block.identify_pool();
-        println!("Block {} ({}) mined by {:?}", hash, height, pool);
+        for tx in block.txdata.iter() {
+            let tx_info = TxInfo::new(tx).unwrap();
+
+            // As demonstration, print txids of transactions which have at least one taproot output
+            // but no taproot script-path spends as input.
+            if tx_info
+                .output_infos
+                .iter()
+                .any(|output| output.out_type == OutputType::P2tr)
+                && !tx_info
+                    .input_infos
+                    .iter()
+                    .any(|input| input.in_type == InputType::P2trsp)
+            {
+                println!("{}", tx.txid());
+            }
+        }
     }
 }
-
